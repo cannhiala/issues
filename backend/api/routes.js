9754df86914bbef9b6da9b8228cabce9af47ae1e
Hoàng Anh Quân <vistaquan@gmail.com>
2020-06-12 08:36:31 +0700

    Quan merge routes.js

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -2,6 +2,7 @@
 
 module.exports = function(app) {
 	let projectCtrl = require('./controllers/ProjectsController');
+	let issueCtrl = require('./controllers/IssuesController');
 
 	app.route('/projects').get(projectCtrl.getProject);
 	app.route('/owner').get(projectCtrl.getOwner);
@@ -11,5 +12,18 @@ module.exports = function(app) {
 	app.route('/projectTypes').get(projectCtrl.getProjectType);
 	app.route('/users').get(projectCtrl.getUsers);
 	app.route('/addUser').post(projectCtrl.addUser);
+	
+	//issues route
+	//create by Quanha
+	app.route('/issueGetProjectByKey')
+		.get(issueCtrl.issueGetProjectByKey);
+	app.route('/issueGetListStatuses')
+		.get(issueCtrl.issueGetListStatuses);
+	app.route('/issueGetUsersSameProjects')
+		.get(issueCtrl.issueGetUsersSameProjects);
+	app.route('/issueGetListPriotities')
+		.get(issueCtrl.issueGetListPriotities);
+	app.route('/issueFilter')
+		.get(issueCtrl.issueFilter);
 
 };
