70c8f3c333db7ec77cbc96a56370d4c024b8cc58
Hoàng Anh Quân <vistaquan@gmail.com>
2020-06-15 14:52:31 +0700

    Fix menu Logout fucntion

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,24 +1,31 @@
-import React, {useContext} from 'react'
-import {UsernNameContext} from './Login'
-import {  NavLink } from "react-router-dom"
+import React, { useContext } from 'react'
+import { UsernNameContext } from './Login'
+import { NavLink } from "react-router-dom"
+import { useHistory } from "react-router-dom";
+import { removeUserSession } from './../utils/Common'
 
-function Menu () {
-
+function Menu() {
+  let history = useHistory();
   const username = useContext(UsernNameContext);
-  console.log(username);
+
+  const onLogout = () => {
+    removeUserSession();
+    history.push('/login');
+  }
+
   return (
     <div>
       <nav class="navbar navbar-inverse">
         <div class="container-fluid">
-            <ul class="nav navbar-nav">
-              <li class="active"><NavLink exact to="/home"> Home </NavLink></li>
-              <li><NavLink exact to="/projects"> Projects </NavLink></li>
-              <li><NavLink exact to="/issues"> Issues </NavLink></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span> {username}</a></li>
-                <li><a href="onClick={onLogout}"><span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
-            </ul>
+          <ul class="nav navbar-nav">
+            <li class="active"><NavLink exact to="/home"> Home </NavLink></li>
+            <li><NavLink exact to="/projects"> Projects </NavLink></li>
+            <li><NavLink exact to="/issues"> Issues </NavLink></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> {username}</a></li>
+            <li><a href="#" onClick={onLogout}><span class="glyphicon glyphicon-log-out"></span> Logout </a></li>
+          </ul>
         </div>
       </nav>
     </div>
