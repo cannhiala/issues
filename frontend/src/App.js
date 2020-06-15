c6396b5e91d864a6f0668d589ba4ffa8c020a07d
Hoàng Anh Quân <vistaquan@gmail.com>
2020-06-15 14:19:32 +0700

    Fix App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,55 @@
-import React, { useState, useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
+import axios from 'axios'
+import PrivateRoute from './utils/PrivateRoute'
+import PublicRoute from './utils/PublicRoute'
+import { getToken, removeUserSession, setUserSession } from './utils/Common'
+
 import Menu from './components/Menu'
-import Login, { UserIDContext } from './components/Login'
-import Home from './components/Home/Homeinterface';
-import ProjectDetail from './components/ProjectDetail';
-import Projects from './components/Projects';
-import CreateProject from './components/CreateProject';
+import Login from './components/Login'
+import Home from './components/Home/Homeinterface'
+import ProjectDetail from './components/ProjectDetail'
+import Projects from './components/Projects'
+import CreateProject from './components/CreateProject'
 import IssueSearchForm from './components/Issues/IssueSearchForm'
 
 
+
+
 function App() {
+  const [isAuthen, setAuth] = useState(true)
 
-  const userId = useContext(UserIDContext);
-  const [isAuthenticated, userHasAuthenticated] = useState(false)
+  useEffect(() => {
+    const token = getToken()
+    if (!token) {
+      return
+    }
 
-  console.log('isAuthenticated: ', isAuthenticated)
-  
+    axios.get(`http://localhost:3001/verifyToken?token=${token}`).then(response => {
+      setUserSession(response.data.token, response.data.user);
+      setAuth(true);
+    }).catch(error => {
+      removeUserSession();
+      setAuth(false);
+    });
+
+
+  })
+
   return (
     <div className="container">
-      {!isAuthenticated ? <Redirect to="/login" /> : <Menu />}
+      {
+        isAuthen ? <Menu /> : <div className="navMenu"></div>
+      }
       <Switch>
-        <Route exact path='/login'>
-          <Login isAuthenticated={isAuthenticated} />
-        </Route>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/projects">
-          <Projects />
-        </Route>
-        <Route exact path="/projects/:succes/:pId">
-          <Projects />
-        </Route>
-        <Route path="/pDetail/:pId">
-          <ProjectDetail />
-        </Route>
-        <Route path="/newProject">
-          <CreateProject />
-        </Route>
-        <Route exact path="/issues">
-          <IssueSearchForm />
-        </Route>
+        <PublicRoute path='/login' component={Login} />
+        <PrivateRoute exact path='/' component={Home} />
+        <PrivateRoute exact path='/home' component={Home} />
+        <PrivateRoute exact path='/projects' component={Projects} />
+        <PrivateRoute exact path='/projects/:succes/:pId' component={Projects} />
+        <PrivateRoute exact path='/pDetail/:pId' component={ProjectDetail} />
+        <PrivateRoute exact path='/newProject' component={CreateProject} />
+        <PrivateRoute exact path='/issues' component={IssueSearchForm} />
       </Switch>
     </div>
   );
