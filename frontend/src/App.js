36294fa76cba50b045da479d2ac5bbb9004672f2
anhquan22 <vistaquan@gmail.com>
2020-06-21 22:16:04 +0700

    Fix private route not work
    Signed-off-by: anhquan22 <vistaquan@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,46 +35,16 @@ function App() {
     <div className="_app">
       <Switch>
         <PublicRoute path='/login' component={Login} />
-        <PrivateRoute exact path='/'>
-          <Menu />
-          <Home />
-        </PrivateRoute>
-        <PrivateRoute exact path='/home'>
-          <Menu />
-          <Home />
-        </PrivateRoute>
-        <PrivateRoute exact path='/projects'>
-          <Menu />
-          <Projects />
-        </PrivateRoute>
-        <PrivateRoute exact path='/projects/:succes/:pId'>
-          <Menu />
-          <Projects />
-        </PrivateRoute>
-        <PrivateRoute exact path='/pDetail/:pId'>
-          <Menu />
-          <ProjectDetail />
-        </PrivateRoute>
-        <PrivateRoute exact path='/newProject'>
-          <Menu />
-          <CreateProject />
-        </PrivateRoute>
-        <PrivateRoute exact path='/pEdit/:pId'>
-          <Menu />
-          <EditProject />
-        </PrivateRoute>
-        <PrivateRoute exact path='/pOverview/:pId'>
-          <Menu />
-          <ProjectOverview />
-        </PrivateRoute>
-        <PrivateRoute exact path='/issues'>
-          <Menu />
-          <IssueSearchForm />
-        </PrivateRoute>
-        <PrivateRoute exact path='/addIssue'>
-          <Menu />
-          <AddIssue />
-        </PrivateRoute>
+        <PrivateRoute exact path='/' component={Home} />
+        <PrivateRoute exact path='/home' component={Home} />
+        <PrivateRoute exact path='/projects' component={Projects} />
+        <PrivateRoute exact path='/projects/:succes/:pId' component={Projects} />
+        <PrivateRoute exact path='/pDetail/:pId' component={ProjectDetail} />
+        <PrivateRoute exact path='/newProject' component={CreateProject} />
+        <PrivateRoute exact path='/pEdit/:pId' component={CreateProject} />
+        <PrivateRoute exact path='/pOverview/:pId' component={ProjectOverview} />
+        <PrivateRoute exact path='/issues' component={IssueSearchForm} />
+        <PrivateRoute exact path='/addIssue' component={AddIssue} />
       </Switch>
     </div>
   );
